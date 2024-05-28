c9fe47ba608c404e37411b34a086cdb5124f06f2
jeff <jeff@moduparking.com>
2024-05-28 16:37:05 +0900

    ticket 리듀서 적용

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import webdcReducer from './webdcStore';
 import envReducer from './envStore';
+import ticketReducer from './ticketStore';
 
 const rootReducer = combineReducers({
   webdc: webdcReducer,
   env: envReducer,
+  ticket: ticketReducer,
 });
 
 const store = configureStore({
