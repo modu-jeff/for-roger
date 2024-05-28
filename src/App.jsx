fae1b9a5af32fadbf4fceb8b62d4737ebae6d093
jeff <jeff@moduparking.com>
2024-05-28 14:41:16 +0900

    로져를 위해서

---
+++ src/App.jsx
@@ -0,0 +1,244 @@
+import { useState } from 'react';
+import './App.css';
+import * as api from './fetching';
+
+function App() {
+  const [envWebdcSeq, setEnvWebdcSeq] = useState(0);
+  const [envPayload, setEnvPayload] = useState({
+    env: {},
+  });
+
+  const [ticketWebdcSeq, setTicketWebdcSeq] = useState(0);
+  const [ticketSeq, setTicketSeq] = useState(0);
+  const [ticketPayload, setTicketPayload] = useState({
+    discountId: '',
+    timeLimit: 0,
+    dDay: 0,
+  });
+
+  const [webdcSeq, setWebdcSeq] = useState(0);
+  const [webdcPayload, setWebdcPayload] = useState({
+    systemSeq: 0,
+    parkinglotSeq: 0,
+  });
+
+  return (
+    <>
+      <h1>ENV 파트</h1>
+      <form>
+        <label>
+          WebdcSeq:{' '}
+          <input
+            type="number"
+            onChange={(e) => setEnvWebdcSeq(Number(e.target.value))}
+          />
+        </label>
+        <br />
+        <br />
+        <label>
+          env:{' '}
+          <textarea
+            onChange={(e) => {
+              setEnvPayload((prev) => ({
+                ...prev,
+                env: e.target.value,
+              }));
+            }}
+            rows="5"
+            cols="30"
+            type="text"
+          />
+        </label>
+        <br />
+        <br />
+        <div>
+          <button
+            type="button"
+            onClick={async (e) => {
+              e.preventDefault();
+              await api.createEnv(envWebdcSeq, envPayload);
+            }}
+          >
+            POST 때리기
+          </button>
+          <button
+            type="button"
+            onClick={async (e) => {
+              e.preventDefault();
+              await api.modifyEnv(envWebdcSeq, envPayload);
+            }}
+          >
+            PUT 때리기
+          </button>
+          <button
+            type="button"
+            onClick={async (e) => {
+              e.preventDefault();
+              await api.deleteEnv(envWebdcSeq);
+            }}
+          >
+            DELETE 때리기
+          </button>
+        </div>
+      </form>
+      <h1>TICKET 파트</h1>
+      <form>
+        <label>
+          WebdcSeq:{' '}
+          <input
+            type="number"
+            onChange={(e) => setTicketWebdcSeq(Number(e.target.value))}
+          />
+        </label>
+        <br />
+        <label htmlFor="">
+          TicketSeq:{' '}
+          <input
+            type="number"
+            onChange={(e) => setTicketSeq(Number(e.target.value))}
+          />
+        </label>
+        <br />
+        <div>payload section</div>
+        <label>
+          discountId:{' '}
+          <input
+            type="text"
+            onChange={(e) =>
+              setTicketPayload((prev) => ({
+                ...prev,
+                discountId: e.target.value,
+              }))
+            }
+          />
+        </label>
+        <br />
+        <label>
+          timeLimit:{' '}
+          <input
+            type="text"
+            onChange={(e) =>
+              setTicketPayload((prev) => ({
+                ...prev,
+                timeLimit: Number(e.target.value),
+              }))
+            }
+          />
+        </label>
+        <br />
+        <label>
+          dDay:{' '}
+          <input
+            type="text"
+            onChange={(e) =>
+              setTicketPayload((prev) => ({
+                ...prev,
+                dDay: Number(e.target.value),
+              }))
+            }
+          />
+        </label>
+        <br />
+        <div>
+          <button
+            type="button"
+            onClick={async (e) => {
+              e.preventDefault();
+              await api.createTicket(ticketWebdcSeq, ticketSeq, ticketPayload);
+            }}
+          >
+            POST 때리기
+          </button>
+          <button
+            type="button"
+            onClick={async (e) => {
+              e.preventDefault();
+              await api.modifyTicket(ticketWebdcSeq, ticketSeq, ticketPayload);
+            }}
+          >
+            PUT 때리기
+          </button>
+          <button
+            type="button"
+            onClick={async (e) => {
+              e.preventDefault();
+              await api.deleteTicket(ticketWebdcSeq, ticketSeq);
+            }}
+          >
+            DELETE 때리기
+          </button>
+        </div>
+      </form>
+      <h1>WEBDC 그 잡채</h1>
+      <form>
+        <label>
+          WebdcSeq:{' '}
+          <input
+            type="number"
+            onChange={(e) => setWebdcSeq(Number(e.target.value))}
+          />
+        </label>
+        <br />
+        <span>payload section</span>
+        <br />
+        <label>
+          systemSeq:{' '}
+          <input
+            type="number"
+            onChange={(e) =>
+              setWebdcPayload((prev) => ({
+                ...prev,
+                systemSeq: Number(e.target.value),
+              }))
+            }
+          />
+        </label>
+        <br />
+        <label>
+          parkinglotSeq:{' '}
+          <input
+            type="number"
+            onChange={(e) =>
+              setWebdcPayload((prev) => ({
+                ...prev,
+                parkinglotSeq: Number(e.target.value),
+              }))
+            }
+          />
+        </label>
+        <br />
+        <div>
+          <button
+            type="button"
+            onClick={async (e) => {
+              e.preventDefault();
+              await api.createWebdc(webdcSeq, webdcPayload);
+            }}
+          >
+            POST 때리기
+          </button>
+          <button
+            type="button"
+            onClick={async (e) => {
+              e.preventDefault();
+              await api.modifyWebdc(webdcSeq, webdcPayload);
+            }}
+          >
+            PUT 때리기
+          </button>
+          <button
+            type="button"
+            onClick={async (e) => {
+              e.preventDefault();
+              await api.deleteWebdc(webdcSeq);
+            }}
+          >
+            DELETE 때리기
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default App;
---
+++ src/fetching.js
@@ -0,0 +1,107 @@
+const url = 'https://adminapi-dev.modudev.cloud/v2/webdc';
+
+// ENV 파트
+export const createEnv = async (webdcSeq, payload) => {
+  console.log('createEnv', webdcSeq, payload);
+  console.log(JSON.stringify(payload));
+  await fetch(`${url}/${webdcSeq}/env`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+};
+
+export const modifyEnv = async (webdcSeq, payload) => {
+  console.log('modifyEnv', webdcSeq, payload);
+  console.log(JSON.stringify(payload));
+  await fetch(`${url}/${webdcSeq}/env`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+};
+
+export const deleteEnv = async (webdcSeq) => {
+  console.log('deleteEnv', webdcSeq);
+  await fetch(`${url}/${webdcSeq}/env`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+// TICKET 파트
+export const createTicket = async (webdcSeq, ticketSeq, payload) => {
+  console.log('createTicket', webdcSeq, ticketSeq, payload);
+  console.log(JSON.stringify(payload));
+  await fetch(`${url}/${webdcSeq}/ticket/${ticketSeq}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+};
+
+export const modifyTicket = async (webdcSeq, ticketSeq, payload) => {
+  console.log('modifyTicket', webdcSeq, ticketSeq, payload);
+  console.log(JSON.stringify(payload));
+  await fetch(`${url}/${webdcSeq}/ticket/${ticketSeq}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+};
+
+export const deleteTicket = async (webdcSeq, ticketSeq) => {
+  console.log('deleteTicket', webdcSeq, ticketSeq);
+  await fetch(`${url}/${webdcSeq}/ticket/${ticketSeq}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+// webdc 파트
+
+export const createWebdc = async (webdcSeq, payload) => {
+  console.log('createWebdc', webdcSeq, payload);
+  console.log(JSON.stringify(payload));
+  await fetch(`${url}/${webdcSeq}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+};
+
+export const modifyWebdc = async (webdcSeq, payload) => {
+  console.log('modifyWebdc', webdcSeq, payload);
+  console.log(JSON.stringify(payload));
+  await fetch(`${url}/${webdcSeq}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+};
+
+export const deleteWebdc = async (webdcSeq) => {
+  console.log('deleteWebdc', webdcSeq);
+  await fetch(`${url}/${webdcSeq}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
