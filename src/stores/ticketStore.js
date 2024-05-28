159db08584ee636781a1f77396c64a25f4d92d8c
jeff <jeff@moduparking.com>
2024-05-28 16:36:58 +0900

    ticket 스토어 정의

---
+++ src/stores/ticketStore.js
@@ -0,0 +1,45 @@
+import { createAction, createReducer } from '@reduxjs/toolkit';
+import { produce } from 'immer';
+
+const SET_TICKET_WEBDC_SEQ = 'ticket/SET_TICKET_WEBDC_SEQ';
+const SET_TICKET_SEQ = 'ticket/SET_TICKET_SEQ';
+const SET_TICKET_PAYLOAD = 'ticket/SET_TICKET_PAYLOAD';
+
+export const setTicketWebdcSeq = createAction(SET_TICKET_WEBDC_SEQ);
+export const setTicketSeq = createAction(SET_TICKET_SEQ);
+export const setTicketPayload = createAction(SET_TICKET_PAYLOAD, (payload) => ({
+  payload,
+}));
+
+const initialState = {
+  ticketWebdcSeq: 0,
+  ticketSeq: 0,
+  ticketPayload: {
+    discountId: '',
+    timeLimit: 0,
+    dDay: 0,
+  },
+};
+
+const ticketReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(setTicketWebdcSeq, (state, { payload }) =>
+      produce(state, (draft) => {
+        draft.ticketWebdcSeq = payload;
+      }),
+    )
+    .addCase(setTicketSeq, (state, { payload }) =>
+      produce(state, (draft) => {
+        draft.ticketSeq = payload;
+      }),
+    )
+    .addCase(setTicketPayload, (state, { payload }) =>
+      produce(state, (draft) => {
+        draft.ticketPayload.discountId = payload.discountId;
+        draft.ticketPayload.timeLimit = payload.timeLimit;
+        draft.ticketPayload.dDay = payload.dDay;
+      }),
+    );
+});
+
+export default ticketReducer;
