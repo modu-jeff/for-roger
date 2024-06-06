c39b447e544a4cb5fa867d2302c94345637de421
jeff <jeff@moduparking.com>
2024-06-06 15:21:47 +0900

    api 주석 처리 제거

--- src/api/socialLogin.ts
+++ src/api/socialLogin.ts
@@ -1,26 +1,26 @@
 export async function sendKakaoAuthCode(code: string) {
   console.log('code', code);
-  // const response = await fetch('http://localhost:4000/auth/login/kakao', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({ code }),
-  // });
-  // const data = await response.json();
-  // return data;
+  const response = await fetch('http://localhost:4000/auth/login/kakao', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ code }),
+  });
+  const data = await response.json();
+  return data;
 }
 
 export async function sendNaverAuthCode(code: string, state: string) {
   console.log('code', code);
   console.log('state', state);
-  // const response = await fetch('http://localhost:4000/auth/login/naver', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({ code, state }),
-  // });
-  // const data = await response.json();
-  // return data;
+  const response = await fetch('http://localhost:4000/auth/login/naver', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ code, state }),
+  });
+  const data = await response.json();
+  return data;
 }
